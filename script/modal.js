08630db591e592b0dcd4d03c7c9f37edd5e97fbb
mdmlb <mlopez0195@gmail.com>
2022-03-20 14:55:36 -0500

    habia una vez una iguana

--- script/modal.js
+++ script/modal.js
@@ -1,6 +1,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//activar agregar evento y link de portafolio
 const modalactive = document.querySelector('.btn');
 const modal = document.querySelector('.modal');
 const dark = document.querySelector('.dark');
@@ -27,5 +28,36 @@ cancelang.addEventListener('click', function () {
   if (dark.classList.contains("dark--active") && modal.classList.contains("modal")) {
     dark.classList.remove("dark--active");
     modal.classList.remove("modal-content")
+  }
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+//trayectoria laboral
+const modalactive2 = document.querySelector('.btnty');
+const modal2 = document.querySelector('.modal2');
+const cancelang2 = document.querySelector('.btncancel2')
+
+modalactive2.addEventListener('click', function () {
+  dark.classList.add("dark--active");
+  modal2.classList.add("modal-content");
+  //console.log();
+});
+
+//exit
+
+//with click on the background
+dark.addEventListener('click', function () {
+  if (dark.classList.contains("dark--active") && modal2.classList.contains("modal2")) {
+    dark.classList.remove("dark--active");
+    modal2.classList.remove("modal-content")
+  }
+});
+
+//click on cancel
+cancelang2.addEventListener('click', function () {
+  if (dark.classList.contains("dark--active") && modal2.classList.contains("modal2")) {
+    dark.classList.remove("dark--active");
+    modal2.classList.remove("modal-content")
   }
 });
