4a70963cc0f9da4fe3ee59044dc6e63fb12d9668
Maria Lopez <mlopez0195@gmail.com>
2022-05-28 22:34:00 -0500

    firebase fixed

--- script/recruiterSelection.js
+++ script/recruiterSelection.js
@@ -18,6 +18,7 @@ var userInfo;
 
 const db = getFirestore();
 
+//roles
 const slideValue = document.querySelector('.range__1');
 const inputSlider = document.querySelector('.range__input1');
 const slideValue2 = document.querySelector('.range__2');
@@ -28,10 +29,59 @@ const slideValue4 = document.querySelector('.range__4');
 const inputSlider4 = document.querySelector('.range__input4');
 const slideValue5 = document.querySelector('.range__5');
 const inputSlider5 = document.querySelector('.range__input5');
+
+//skills
+const skill1 = document.querySelector('.rangeskills__1');
+const inputskill1 = document.querySelector('.rangeskills__input1');
+const skill2 = document.querySelector('.rangeskills__2');
+const inputskill2 = document.querySelector('.rangeskills__input2');
+const skill3 = document.querySelector('.rangeskills__3');
+const inputskill3 = document.querySelector('.rangeskills__input3');
+const skill4 = document.querySelector('.rangeskills__4');
+const inputskill4 = document.querySelector('.rangeskills__input4');
+const skill5 = document.querySelector('.rangeskills__5');
+const inputskill5 = document.querySelector('.rangeskills__input5');
+const skill6 = document.querySelector('.rangeskills__6');
+const inputskill6 = document.querySelector('.rangeskills__input6');
+const skill7 = document.querySelector('.range_7');
+const inputskill7 = document.querySelector('.range__input7');
+const skill8 = document.querySelector('.range_8');
+const inputskill8 = document.querySelector('.range__input8');
+const skill9 = document.querySelector('.range_9');
+const inputskill9 = document.querySelector('.range__input9');
+const skill10 = document.querySelector('.range_10');
+const inputskill10 = document.querySelector('.range__input10');
+const skill11 = document.querySelector('.range_11');
+const inputskill11 = document.querySelector('.range__input11');
+const skill12 = document.querySelector('.range_12');
+const inputskill12 = document.querySelector('.range__input12');
+const skill13 = document.querySelector('.range_13');
+const inputskill13 = document.querySelector('.range__input13');
+const skill14 = document.querySelector('.range_14');
+const inputskill14 = document.querySelector('.range__input14');
+const skill15 = document.querySelector('.range_15');
+const inputskill15 = document.querySelector('.range__input15');
+const skill16 = document.querySelector('.range_16');
+const inputskill16 = document.querySelector('.range__input16');
+const skill17 = document.querySelector('.range_17');
+const inputskill17 = document.querySelector('.range__input17');
+const skill18 = document.querySelector('.range_18');
+const inputskill18 = document.querySelector('.range__input18');
+const skill19 = document.querySelector('.range_19');
+const inputskill19 = document.querySelector('.range__input19');
+const skill20 = document.querySelector('.range_20');
+const inputskill20 = document.querySelector('.range__input20');
+const skill21 = document.querySelector('.range_21');
+const inputskill21 = document.querySelector('.range__input21');
+const skill22 = document.querySelector('.range_22');
+const inputskill22 = document.querySelector('.range__input22');
+
 const nameProjectElem = document.querySelector('.nameProject');
 let skillObj = document.querySelector('.range__form');
 
 //cambiar número del slider
+
+//roles
 inputSlider.oninput = (() => {
   let value = inputSlider.value;
   slideValue.textContent = value + "%";
@@ -52,6 +102,97 @@ inputSlider5.oninput = (() => {
   let value = inputSlider5.value;
   slideValue5.textContent = value + "%";
 });
+
+//skills
+inputskill1.oninput = (() => {
+  let value = inputskill1.value;
+  skill1.textContent = value + "%";
+});
+inputskill2.oninput = (() => {
+  let value = inputskill2.value;
+  skill2.textContent = value + "%";
+});
+inputskill3.oninput = (() => {
+  let value = inputskill3.value;
+  skill3.textContent = value + "%";
+});
+inputskill4.oninput = (() => {
+  let value = inputskill4.value;
+  skill4.textContent = value + "%";
+});
+inputskill5.oninput = (() => {
+  let value = inputskill5.value;
+  skill5.textContent = value + "%";
+});
+inputskill6.oninput = (() => {
+  let value = inputskill6.value;
+  skill6.textContent = value + "%";
+});
+inputskill7.oninput = (() => {
+  let value = inputskill7.value;
+  skill7.textContent = value + "%";
+});
+inputskill8.oninput = (() => {
+  let value = inputskill8.value;
+  skill8.textContent = value + "%";
+});
+inputskill9.oninput = (() => {
+  let value = inputskill9.value;
+  skill9.textContent = value + "%";
+});
+inputskill10.oninput = (() => {
+  let value = inputskill10.value;
+  skill10.textContent = value + "%";
+});
+inputskill11.oninput = (() => {
+  let value = inputskill11.value;
+  skill11.textContent = value + "%";
+});
+inputskill12.oninput = (() => {
+  let value = inputskill12.value;
+  skill12.textContent = value + "%";
+});
+inputskill13.oninput = (() => {
+  let value = inputskill13.value;
+  skill13.textContent = value + "%";
+});
+inputskill14.oninput = (() => {
+  let value = inputskill14.value;
+  skill14.textContent = value + "%";
+});
+inputskill15.oninput = (() => {
+  let value = inputskill15.value;
+  skill15.textContent = value + "%";
+});
+inputskill16.oninput = (() => {
+  let value = inputskill16.value;
+  skill16.textContent = value + "%";
+});
+inputskill17.oninput = (() => {
+  let value = inputskill17.value;
+  skill17.textContent = value + "%";
+});
+inputskill18.oninput = (() => {
+  let value = inputskill18.value;
+  skill18.textContent = value + "%";
+});
+inputskill19.oninput = (() => {
+  let value = inputskill19.value;
+  skill19.textContent = value + "%";
+});
+inputskill20.oninput = (() => {
+  let value = inputskill20.value;
+  skill20.textContent = value + "%";
+});
+inputskill21.oninput = (() => {
+  let value = inputskill21.value;
+  skill21.textContent = value + "%";
+});
+inputskill22.oninput = (() => {
+  let value = inputskill22.value;
+  skill22.textContent = value + "%";
+});
+
 
 
 let uxDesiredProfiles = [];
@@ -84,10 +225,122 @@ skillObj.addEventListener('submit', function (event) {
     value: Number(inputSlider5.value),
   }
 
+  //guardar en objetos los datos de los habilidades
+  const comunicacionoral  = {
+    nameRole: "Comunicación oral",
+    value: Number(inputskill1.value),
+  }
+
+  const comunicacionescrita = {
+    nameRole: "Comunicación escrita",
+    value: Number(inputskill2.value),
+  }
+
+  const empatia = {
+    nameRole: "Empatía",
+    value: Number(inputskill3.value),
+  }
+
+  const trabajoenequipo = {
+    nameRole: "Trabajo en equipo",
+    value: Number(inputskill4.value),
+  }
+  
+  const analisisdedatos = {
+    nameRole: "Análisis de datos",
+    value: Number(inputskill5.value),
+  }
+
+  const escuchaactiva  = {
+    nameRole: "Escucha activa",
+    value: Number(inputskill1.value),
+  }
+
+  const negociacion = {
+    nameRole: "Negociación",
+    value: Number(inputskill2.value),
+  }
+
+  const actitudcritica = {
+    nameRole: "Actitud crítica",
+    value: Number(inputskill3.value),
+  }
+
+  const conocimientosenpsicologia = {
+    nameRole: "Conocimientos en psicología",
+    value: Number(inputskill4.value),
+  }
+
+  const visionalfuturo = {
+    nameRole: "Visión al futuro",
+    value: Number(inputskill5.value),
+  }
+
+  const capacidaddegestion  = {
+    nameRole: "Capacidad de gestión",
+    value: Number(inputskill1.value),
+  }
+
+  const comunicaciónvisual = {
+    nameRole: "Comunicación visual",
+    value: Number(inputskill2.value),
+  }
+
+  const manejodeltiempo = {
+    nameRole: "Manejo del tiempo",
+    value: Number(inputskill3.value),
+  }
+  
+  const Adaptaciónalcambio = {
+    nameRole: "Adaptación al cambio",
+    value: Number(inputskill5.value),
+  }
+
+  const prototipado  = {
+    nameRole: "Prototipado",
+    value: Number(inputskill1.value),
+  }
+
+  const estructuracióndelainformacion = {
+    nameRole: "Estructuración de la información",
+    value: Number(inputskill2.value),
+  }
+
+  const creatividad = {
+    nameRole: "Creatividad",
+    value: Number(inputskill3.value),
+  }
+
+  const conocimientosdeinteraccion = {
+    nameRole: "Conocimientos de interacción",
+    value: Number(inputskill4.value),
+  }
+
+  const resistenciaaltrabajobajopresion = {
+    nameRole: "Resistencia al trabajo bajo presión",
+    value: Number(inputskill5.value),
+  }
+  
+  const liderazgo = {
+    nameRole: "Liderazgo",
+    value: Number(inputskill3.value),
+  }
+
+  const investigacioncuantitativa = {
+    nameRole: "Investigación cuantitativa",
+    value: Number(inputskill4.value),
+  }
+
+  const investigacioncualitativa = {
+    nameRole: "Investigación cualitativa",
+    value: Number(inputskill5.value),
+  }
+  
+
   const nameProject = nameProjectElem.value;
 
   //guardar en arreglo los objetos anteriores
-  uxDesiredProfiles.push(nameProject, researcher, content, interaction, ui, lead);
+  uxDesiredProfiles.push(nameProject, researcher, content, interaction, ui, lead, comunicacionoral, comunicacionescrita, empatia, trabajoenequipo, analisisdedatos, escuchaactiva, negociacion, actitudcritica, conocimientosenpsicologia, visionalfuturo, capacidaddegestion, comunicaciónvisual, manejodeltiempo, Adaptaciónalcambio, prototipado, estructuracióndelainformacion, creatividad, conocimientosdeinteraccion, resistenciaaltrabajobajopresion, liderazgo, investigacioncuantitativa, investigacioncualitativa);
 
   onAuthStateChanged(auth, async (user) => {
     if (user) {
@@ -122,7 +375,7 @@ skillObj.addEventListener('submit', function (event) {
             })
 
               .then(function () {
-                window.location.href = './recommendations.html';
+                window.location.href = './projects.html';
               });
           }
 
@@ -135,7 +388,7 @@ skillObj.addEventListener('submit', function (event) {
             })
 
               .then(function () {
-                window.location.href = './recommendations.html';
+                window.location.href = './projects.html';
               });
           }
 
